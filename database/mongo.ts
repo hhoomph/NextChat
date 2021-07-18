f15e3fcde1dcbb6ec37e61bff9696a6fe1ac4608
hhoomph <hh.oomph@gmail.com>
2021-07-18 23:31:32 +0430

    fix mongodb URL

--- database/mongo.ts
+++ database/mongo.ts
@@ -4,7 +4,7 @@ const dbName = process.env.DBName;
 const dbUrl =
   process.env.NODE_ENV !== "production"
     ? process?.env?.DATABASE_URL.toString() + dbName
-    : `mongodb+srv://oomph:<${process?.env?.DBPass}>@cluster0.xqf1t.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+    : `mongodb+srv://oomph:${process?.env?.DBPass}@cluster0.xqf1t.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 // connecting to mongo
 // const mongoDb = mongoose.connect(dbUrl + dbName, {
 //   useNewUrlParser: true,
