e0547235b908e87d696760b704f9a5e969e7cd6c
hhoomph <hh.oomph@gmail.com>
2021-09-14 14:51:28 +0430

    change about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,12 +5,13 @@ const AboutPage = () => (
       <h1 className="text-center">درباره ما</h1>
       <div className="col-sm-12 col-md-8 col-lg-9">
         <h4>پروژه چت</h4>
-        <p>پروژه چت پیاده سازی شده با React , Nextjs, Socket.io و MongoDB.</p> <br />
+        <p>پروژه چت پیاده سازی شده با React , Nextjs, Socket.io و MongoDB و WebRTC.</p> <br />
         <h5>امکانات : </h5>
         <ul>
           <li>امکان ثبت نام و لاگین</li>
           <li>قابلیت جستجوی کاربران</li>
           <li>امکان چت همزمان</li>
+          <li>امکان مکالمه صوتی و تصویری</li>
           <li>ذخیره پیام ها و دریافت آن ها در حالت آفلاین</li>
           <li>نمایش وضعیت آنلاین و آفلاین</li>
           <li>قابلیت استفاده از emoji</li>
@@ -18,7 +19,8 @@ const AboutPage = () => (
         <small className="text-muted">
           طراحی و پیاده سازی توسط
           <a className="footerColor" href="mailto:hh.oomph@gmail.com">
-          {" "} H.H {" "}
+            {" "}
+            H.H{" "}
           </a>
         </small>
       </div>
