89c7aa1c7ff803ea7aea090b1d62fc17bdc4a5a3
hhoomph <hh.oomph@gmail.com>
2024-03-05 14:10:19 +0330

    change domain

--- server/index - Copy.ts
+++ server/index - Copy.ts
@@ -69,7 +69,7 @@ app
     const io = new Server(httpServer, socketOption);
     io.use(function (socket: ISocket, next) {
       if (socket.handshake.query && socket.handshake.query.token) {
-        jwt.verify(socket.handshake.query.token.toString(), SECRET, function (err, decoded) {
+        jwt.verify(socket.handshake.query.token.toString(), SECRET, function (err, decoded:any) {
           if (err) return next(new Error("Authentication error"));
           if (decoded?.user) {
             decoded.user.ID = socket.id;
