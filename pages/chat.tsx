e5aec3c7e8787069296b5256e8f1b74bfcadb879
hhoomph <hh.oomph@gmail.com>
2024-03-05 14:18:50 +0330

    add jsonp to socketIo

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -114,6 +114,7 @@ const ChatPage: NextPage<Props> = ({ user, props }: Props) => {
     multiplex: false,
     transports: ["websocket"],
     upgrade: false,
+    jsonp: false,
     reconnection: true,
     reconnectionDelay: 500,
   });
